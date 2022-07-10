c5a2eb0de8c13e3a1e2966f004a8ff7769d5dcc7
plejeail <pierre.lejeail@hotmail.fr>
2022-07-10 17:19:42 +0200

    benchmark: better Workflow, fix stopwatch/TimeSpan != clock precision

--- FeaturlessLab/BenchmarkLab.cs
+++ FeaturlessLab/BenchmarkLab.cs
@@ -1,12 +1,24 @@
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable UnusedVariable
 namespace FeaturlessLab;
 
 using Featurless;
 
-public class BenchmarkLab
+public static class BenchmarkLab
 {
     private static void Sum(int[] arr) {
         for (int i = 1; i < arr.Length; ++i) {
             arr[i] += arr[i-1];
+        }
+    }
+
+    private static unsafe void Sum2(int[] arr) {
+        fixed (int* arrPtrFixed = arr) {
+            int* arrPtr = arrPtrFixed;
+            for (int i = 1; i < arr.Length; ++i) {
+                *(++arrPtr) += *arrPtr;
+            }
         }
     }
 
@@ -16,24 +28,44 @@ public class BenchmarkLab
         }
     }
 
+    private static unsafe void Mult2(int[] arr) {
+        fixed (int* arrPtrFixed = arr) {
+            int* arrPtr = arrPtrFixed;
+            for (int i = 2; i < arr.Length; ++i) {
+                *(++arrPtr) *= *arrPtr;
+            }
+        }
+    }
+
     private static void None() {}
 
-    public static void Run() {
-        Benchmark bench = new();
-
-        int[] arrayInt = new int[20000];
+    private static int[] CreateArray(int size = 20000) {
+        int[] arrayInt = new int[size];
         for (int i = 0; i < arrayInt.Length; ++i) {
             arrayInt[i] = i;
         }
 
+        return arrayInt;
+    }
 
-        bench.Run("test", "sum 1", () => Sum(arrayInt));
-        bench.Run("test", "sum 1", () => Sum(arrayInt));
-        bench.Run("test", "mult",  () => Mult(arrayInt));
-        bench.Run("test", "mult", () => Mult(arrayInt));
-        bench.Run("test", "sum 2", () => Sum(arrayInt));
-        bench.Run("test", "nothing", None);
-        bench.Run("test", "nothing", None);
+    public static void Run() {
+        Benchmark bench = new();
+        Thread.Sleep(20);
+
+        int[] arr1 = CreateArray(200);
+        int[] arr2 = CreateArray(200);
+        int[] arr3 = CreateArray(200);
+        int[] arr4 = CreateArray(200);
+ //      bench.Run("test sum", "sum 1", () => Sum(arr1));
+ //      bench.Run("test mult", "mult 1", () => Mult(arr2));
+ //      bench.Run("test sum", "sum 2", () => Sum2(arr3));
+ //      bench.Run("test mult", "mult 2",  () => Mult2(arr4));
+ //      bench.Run("test sum", "sum 1", () => Sum(arr1));
+ //      bench.Run("test mult", "mult 1", () => Mult(arr2));
+        bench.Run("test sum", "sum 2", () => Sum2(arr3));
+        bench.Run("test mult", "mult 2", () => Mult2(arr4));
+        bench.Run("test mult", "None", None);
+        bench.Run("test sum", "None", None);
 
         Console.WriteLine(bench);
     }
